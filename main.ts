6f89e5917442b2efb9c9afe1e235a78b29d98c93
daperedondo <daperedondo@hotmail.com>
2022-01-19 12:28:10 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,33 @@
+makerbit.onIrButton(IrButton.Up, IrButtonAction.Released, function () {
+    basic.showLeds(`
+        . . . . .
+        . # . # .
+        . . . . .
+        . # # # .
+        # . . . #
+        `)
+    RobotCar_Keyestudio.Motors.stop()
+})
+makerbit.onIrButton(IrButton.Down, IrButtonAction.Pressed, function () {
+    basic.showLeds(`
+        . . . . .
+        . # . # .
+        . . . . .
+        # . . . #
+        . # # # .
+        `)
+    RobotCar_Keyestudio.Motors.move(-50)
+})
+makerbit.onIrButton(IrButton.Down, IrButtonAction.Released, function () {
+    basic.showLeds(`
+        . . . . .
+        . # . # .
+        . . . . .
+        . # # # .
+        # . . . #
+        `)
+    RobotCar_Keyestudio.Motors.stop()
+})
 makerbit.onIrButton(IrButton.Up, IrButtonAction.Pressed, function () {
     basic.showLeds(`
         . . . . .
@@ -6,6 +36,29 @@ makerbit.onIrButton(IrButton.Up, IrButtonAction.Pressed, function () {
         # . . . #
         . # # # .
         `)
+    RobotCar_Keyestudio.Motors.steer(50, 0)
+    RobotCar_Keyestudio.Motors.move(50)
+})
+makerbit.onIrButton(IrButton.Right, IrButtonAction.Pressed, function () {
+    basic.showLeds(`
+        . . . . .
+        . . # . #
+        . . . . .
+        . . # . .
+        . . . # #
+        `)
+    RobotCar_Keyestudio.Motors.spin(100)
+    RobotCar_Keyestudio.Motors.move(50)
+})
+makerbit.onIrButton(IrButton.Ok, IrButtonAction.Pressed, function () {
+    basic.showLeds(`
+        . . . . .
+        . # . # .
+        . . . . .
+        . # # # .
+        # . . . #
+        `)
+    RobotCar_Keyestudio.Motors.stop()
 })
 makerbit.connectIrReceiver(DigitalPin.P16, IrProtocol.Keyestudio)
 basic.showLeds(`
